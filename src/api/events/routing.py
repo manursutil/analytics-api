cf04835aef8545b3e08cc5cd2b98a27c51ab3ad5
Manuel Rodríguez <manuel@MacBook-Air-de-Manuel.local>
2025-07-08 20:10:33 +0100

    incoming data validation

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,15 +1,30 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import EventSchema, EventListSchema, EventCreateSchema, EventUpdateSchema
 
 router = APIRouter()
 
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "results": [1, 2, 3]
-    }
+        "results": [{"id": 1}, {"id": 2}, {"id": 3}],
+        "count": 3
+    } # type: ignore
+    
+@router.post("/")
+def create_event(payload: EventCreateSchema) -> EventListSchema:
+    print(payload.page)
+    return {
+        "results": [{"id": 123}], 
+        "count": 1
+    } # type: ignore
     
 
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
+    return {"id": event_id} # type: ignore
+
+
+@router.put("/{event_id}")
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    print(payload.description)
     return {"id": event_id} # type: ignore
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,5 +1,15 @@
+from typing import List
 from pydantic import BaseModel
+
+class EventCreateSchema(BaseModel):
+    page: str
+    
+class EventUpdateSchema(BaseModel):
+    description: str
 
 class EventSchema(BaseModel):
     id: int
     
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
+    count: int
